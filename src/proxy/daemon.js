0d3c6c7dd85d373facdda9814814eef3dc5daa91
Alexandre Amaral de Carvalho <ajscarvalho@gmail.com>
2012-11-17 00:56:49 +0000

    fixing memory leaks?

--- src/proxy/daemon.js
+++ src/proxy/daemon.js
@@ -25,16 +25,19 @@ var proxy = http.createServer(
 
 		switch(action)
 		{
+			case 'subscribe': 
+				if (!param) return http_error(response, 500, 'parameter required');
+				if (instanceList[id]) delete instanceList[id];
+				instanceList[id] = new WebClient(id);
+
 			case 'setPlayerName':
 			case 'setAnswer': 
-			case 'subscribe': 
 				if (!param) return http_error(response, 500, 'parameter required');
 
 			case 'listPlayers':
 			case 'newQuestion':
 			case 'timeEnded':
 			case 'setReadyToPlay':
-				if (action == 'subscribe') instanceList[id] = new WebClient(id);
 				if (!instanceList[id]) return http_error(response, 500, 'not ready yet! have you subscribed?');
 				var res = instanceList[id].send(action, param);
 				if (!res) return http_error(response, 500, 'not ready yet! have you subscribed?');
