373f7722321de7fd73f3f246295e54ab7ee841b4
Bruno Carreira <bruno.d.carreira@telecom.pt>
2013-09-25 01:28:07 +0100

    use bk

--- src/logic/Musicbozz/Catalog/Musicbox/PublicApi.php
+++ src/logic/Musicbozz/Catalog/Musicbox/PublicApi.php
@@ -5,7 +5,7 @@ final class PublicApi extends AbstractBaseClient {
 	
 	public function __construct()
 	{
-		$this->actor = "https://services.sapo.pt/Music/OnDemand/PublicApi";
+		$this->actor = "https://services.bk.sapo.pt/Music/OnDemand/PublicApi";
 		$this->wsdl = __DIR__ . '/Contracts/Music_OnDemand_PublicApi.wsdl'; //@todo
 
 		parent::__construct();
