42ec7dfcde36c3d6435fa6256713cdd04acacafa
Bruno Carreira <bruno.d.carreira@telecom.pt>
2013-09-25 02:28:22 +0100

    facebook integration

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -267,36 +267,46 @@ var musicbozz = (function(){
 		$(document).delegate('a', 'click', function(e){e.preventDefault();});
 		$(document).delegate('form', 'submit', function(e){e.preventDefault();});
 
-		FB.Event.subscribe('auth.authResponseChange', function(response) {
-            // Here we specify what we do with the response anytime this event occurs. 
-            if (response.status === 'connected') {
-              // The response object is returned with a status field that lets the app know the current
-              // login status of the person. In this case, we're handling the situation where they 
-              // have logged in to the app.
-              FB.api('/me', function(response) {
-	              console.log(response.name);
-	              playerName = response.name;
-	              $joinButton.removeAttr('disable');
-	            });
-            } else if (response.status === 'not_authorized') {
-              // In this case, the person is logged into Facebook, but not into the app, so we call
-              // FB.login() to prompt them to do so. 
-              // In real-life usage, you wouldn't want to immediately prompt someone to login 
-              // like this, for two reasons:
-              // (1) JavaScript created popup windows are blocked by most browsers unless they 
-              // result from direct interaction from people using the app (such as a mouse click)
-              // (2) it is a bad experience to be continually prompted to login upon page load.
-              
-              FB.login();
-            } else {
-              // In this case, the person is not logged into Facebook, so we call the login() 
-              // function to prompt them to do so. Note that at this stage there is no indication
-              // of whether they are logged into the app. If they aren't then they'll see the Login
-              // dialog right after they log in to Facebook. 
-              // The same caveats as above apply to the FB.login() call here.
-              FB.login();
-            }
-          });
+		window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '667899479901883', // App ID
+                channelUrl : '//vmdev-musicbozz.vmdev.bk.sapo.pt/channel.html', // Channel File
+                status     : true, // check login status
+                cookie     : true, // enable cookies to allow the server to access the session
+                xfbml      : true  // parse XFBML
+              });
+          
+			FB.Event.subscribe('auth.authResponseChange', function(response) {
+	            // Here we specify what we do with the response anytime this event occurs. 
+	            if (response.status === 'connected') {
+	              // The response object is returned with a status field that lets the app know the current
+	              // login status of the person. In this case, we're handling the situation where they 
+	              // have logged in to the app.
+	              FB.api('/me', function(response) {
+		              console.log(response.name);
+		              playerName = response.name;
+		              $joinButton.removeAttr('disable');
+		            });
+	            } else if (response.status === 'not_authorized') {
+	              // In this case, the person is logged into Facebook, but not into the app, so we call
+	              // FB.login() to prompt them to do so. 
+	              // In real-life usage, you wouldn't want to immediately prompt someone to login 
+	              // like this, for two reasons:
+	              // (1) JavaScript created popup windows are blocked by most browsers unless they 
+	              // result from direct interaction from people using the app (such as a mouse click)
+	              // (2) it is a bad experience to be continually prompted to login upon page load.
+
+	              FB.login();
+	            } else {
+	              // In this case, the person is not logged into Facebook, so we call the login() 
+	              // function to prompt them to do so. Note that at this stage there is no indication
+	              // of whether they are logged into the app. If they aren't then they'll see the Login
+	              // dialog right after they log in to Facebook. 
+	              // The same caveats as above apply to the FB.login() call here.
+	              FB.login();
+	            }
+          	});
+		};
         
 	});
 
