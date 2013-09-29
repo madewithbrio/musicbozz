29f982105a8497e16d610b45eaabbd2e9ce6278c
Bruno Carreira <bcarreira@bcarreira-Satellite-A300.(none)>
2013-09-30 00:17:28 +0100

    add semicols

--- web/static/js/musicbozz.js
+++ web/static/js/musicbozz.js
@@ -7,7 +7,7 @@ window.fbAsyncInit = function() {
         cookie     : true, // enable cookies to allow the server to access the session
         xfbml      : true  // parse XFBML
       });
-}
+};
 
 var musicbozz = (function(facebookSDK){
 	'use strict';
@@ -50,21 +50,21 @@ var musicbozz = (function(facebookSDK){
 			this.question = undefined;
 			this.roomName = roomName || this.player.username;
 			this.master = !roomName;
-		}
+		};
 
-		Room.prototype.addPlayer = function() {}
-		Room.prototype.setMaster = function(master) { this.master; }
-		Room.prototype.getPlayer = function() { return this.player; }
-		Room.prototype.getQuestion = function() {}
-		Room.prototype.isAlone = function() { return this.type == 'alone'; }
-		Room.prototype.isMaster = function () { return this.master; }
+		Room.prototype.addPlayer = function() {};
+		Room.prototype.setMaster = function(master) { this.master; };
+		Room.prototype.getPlayer = function() { return this.player; };
+		Room.prototype.getQuestion = function() {};
+		Room.prototype.isAlone = function() { return this.type == 'alone'; };
+		Room.prototype.isMaster = function () { return this.master; };
 		Room.prototype.getRoomId = function() {
 			return ((this.type == 'alone') ? 'alone/' : 'room/') + this.roomName;
-		}
+		};
 		Room.prototype.getLocation = function() {
 			return 'ws://vmdev-musicbozz.vmdev.bk.sapo.pt/ws/'
 				 + this.getRoomId();
-		}
+		};
 
 		return Room;
 	})();
@@ -79,17 +79,17 @@ var musicbozz = (function(facebookSDK){
 			if (alone) $room.addClass('alone');
 			$room.show();
 			$homepage.hide();
-		}
+		};
 
 		view.showHomepage = function() {
 			$homepage.show();
 			$room.hide();
-		}
+		};
 
 		view.renderGamestart = function() {
 			$('#standBy').hide();
 			$('#gameBoard').show();
-		}
+		};
 
 		view.renderPlayers = function(res) {
 	        	$('ul[data-template="players"]').html(Mustache.render(getTemplate('players'), {players: res}, getTemplate()));
@@ -136,10 +136,10 @@ var musicbozz = (function(facebookSDK){
 			clazzName =  data.res ? 'correct' : 'wrong';
 			this.liElement.children().addClass('selected ' + clazzName);
 			this.liElement.parent().addClass('has_answer');
-		}
+		};
 
 		var getTemplate = function(name) {
-			if (typeof name === "string" && typeof partialTemplates[name] === "string") { return partialTemplates[name] }
+			if (typeof name === "string" && typeof partialTemplates[name] === "string") { return partialTemplates[name]; }
 			else if (typeof name === "string") throw "template not defined";
 			return partialTemplates;
 		};
@@ -170,7 +170,7 @@ var musicbozz = (function(facebookSDK){
 	var controller = (function() {
 		var controller = {}, hasAnswer = false, timeoutQuestion,
 			errorHandling = function(error, desc){ 
-				console.error(error, desc) 
+				console.error(error, desc); 
 			};
 
 		controller.goRoom = function(type, roomName) {
@@ -187,7 +187,7 @@ var musicbozz = (function(facebookSDK){
 			    	controller.login(type, roomName);
 			  	}
 			});
-		}
+		};
 
 		controller.login = function(type, roomName) {
 			facebookSDK.Event.subscribe('auth.authResponseChange', function(response) {
@@ -198,19 +198,19 @@ var musicbozz = (function(facebookSDK){
 		        }
 		  	});
 		  	facebookSDK.login();
-		}
+		};
 
 		controller.startGame = function() {
 			service.getNewQuestion(roomInstance, function(question) {
 				view.renderGamestart();
 				//view.renderQuestion(question);
 			}, errorHandling);
-		}
+		};
 
 		controller.newQuestion = function(question) {
 			hasAnswer = false;
 			view.renderQuestion(question);
-		}
+		};
 
 		controller.startAudioPlayer = function() {
 			console.log("start");
@@ -218,11 +218,11 @@ var musicbozz = (function(facebookSDK){
 				console.log(player);
 				player.play(); 
 			}, 250);
-		}
+		};
 
 		controller.playersAnswerResult = function(result) {
 			view.renderPlayersAnswerResult(result);
-		}
+		};
 
 		controller.questionOver = function() {
 			player.pause();
@@ -230,7 +230,7 @@ var musicbozz = (function(facebookSDK){
 			view.cleanPlayesrAnswerNotifications();
 			if(roomInstance.isMaster()) service.getNewQuestion(roomInstance);
 		//	if (result.isOver) { $('div.question').html(""); }
-		}
+		};
 
 		controller.setAnswer = function(el) {
 			var $li = $(el).parent(), hash = $('div[data-template="question"] .query').attr('data-hash'),
@@ -241,7 +241,7 @@ var musicbozz = (function(facebookSDK){
 			$('a[data-element="answer"]').unbind('click.answer');
 
 			service.setAnswer(roomInstance, answer, hash, view.renderPlayerAnswer.bind({liElement: $li}));
-		}
+		};
 
 		controller.eventHandler = function(t, e) {
 			switch (e.action) {
@@ -280,7 +280,7 @@ var musicbozz = (function(facebookSDK){
 				default:
 					break;
 			}
-		}
+		};
 
 		// bind gui components
 		// prevent default all link and submit actions
@@ -304,7 +304,7 @@ var musicbozz = (function(facebookSDK){
 		var service = {}, ws_session, playerConfig;
 		service.getPlayer = function() {
 			return playerConfig;
-		}
+		};
 
 		service.loadFacebookPersona = function(onResponseClb) {
 			facebookSDK.api('/me', function(response) {
@@ -314,10 +314,10 @@ var musicbozz = (function(facebookSDK){
 	          	avatar: 'http://graph.facebook.com/'+response.username+'/picture',
 	          	username: response.username,
 	          	link: response.link
-	          }
+	          };
 	          if (typeof onResponseClb === 'function') onResponseClb.apply(null, playerConfig);
 	        });
-		}
+		};
 
 		service.connect = function(gameRoom) {
 			ab.connect(gameRoom.getLocation(), function(session){
@@ -331,42 +331,42 @@ var musicbozz = (function(facebookSDK){
 			}, function(){
 				console.log("session closed");
 			});
-		}
+		};
 
 		service.listPlayers = function(gameRoom, onSuccess, onError) {
 			if (typeof onSuccess !== 'function') onSuccess = function(){};
 			if (typeof onError !== 'function') onError = function(){};
 			
 			ws_session.call(gameRoom.getRoomId(), 'listPlayers').then(onSuccess, onError);
-		}
+		};
 
 		service.getNewQuestion = function (gameRoom, onSuccess, onError) {
 			if (typeof onSuccess !== 'function') onSuccess = function(){};
 			if (typeof onError !== 'function') onError = function(){};
 			
 			ws_session.call(gameRoom.getRoomId(), 'getNewQuestion').then(onSuccess, onError);
-		}
+		};
 
 		service.setReadyToPlay = function (gameRoom, hash, onSuccess, onError) {
 			if (typeof onSuccess !== 'function') onSuccess = function(){};
 			if (typeof onError !== 'function') onError = function(){};
 			
 			ws_session.call(gameRoom.getRoomId(), 'setReadyToPlay', {hash: hash}).then(onSuccess, onError);
-		}
+		};
 
 		service.notifyTimeEnded = function (gameRoom, hash, onSuccess, onError) {
 			if (typeof onSuccess !== 'function') onSuccess = function(){};
 			if (typeof onError !== 'function') onError = function(){};
 			
 			ws_session.call(gameRoom.getRoomId(), 'timeEnded', {answer: null, hash: hash}).then(onSuccess, onError);
-		}
+		};
 
 		service.setAnswer = function (gameRoom, answer, hash, onSuccess, onError) {
 			if (typeof onSuccess !== 'function') onSuccess = function(){};
 			if (typeof onError !== 'function') onError = function(){};
 
 			ws_session.call(gameRoom.getRoomId(), 'setAnswer', {answer: answer, hash: hash}).then(onSuccess, onError);
-		}
+		};
 
 		
 		return service;
@@ -391,5 +391,5 @@ var musicbozz = (function(facebookSDK){
 		getView: function() {
 			return view;
 		}
-	}
+	};
 })(FB);
