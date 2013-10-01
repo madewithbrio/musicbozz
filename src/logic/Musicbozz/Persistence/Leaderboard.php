9dc2bf09159369f5af9b48fb51af1d509f1510c2
Bruno Carreira <bruno.d.carreira@telecom.pt>
2013-10-01 18:30:31 +0100

    .

--- src/logic/Musicbozz/Persistence/Leaderboard.php
+++ src/logic/Musicbozz/Persistence/Leaderboard.php
@@ -3,5 +3,9 @@
 namespace Musicbozz\Persistence;
 
 class Leaderboard {
+	const ALONE = 'leaderboard::alone';
+	const ROOMPRIVATE = 'leaderboard::private';
+	const ROOMPUBLIC = 'leaderboard::public';
+
 	
 }
