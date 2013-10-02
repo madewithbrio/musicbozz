58d273c334c570793145530a37b29227d395d31a
Bruno Carreira <bruno.d.carreira@telecom.pt>
2013-10-02 12:37:01 +0100

    add cache for lastFM

---
+++ src/logic/Musicbozz/Persistence/SimilarArtist.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Musicbozz\Persistence;
+use \Sapo\Redis;
+
+class SimilarArtist {
+	const HASHKEY = 'SimilarArtist';
+
+	public static function save($key, $object) {
+		Redis::getInstance()->hset(self::HASHKEY, $key, serialize($object));
+	}
+
+	public static function get($key) {
+		$serializeObj = Redis::getInstance()->hget(self::HASHKEY, $key);
+		return unserialize($serializeObj);
+	}
+
+	public static function delete($key) {
+		Redis::getInstance()->hdel(self::HASHKEY, $key);
+	}
+}
